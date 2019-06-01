12f0fbcbe0cbdbafabf994426b7e6d93219e120b
Lisa Lemons <lisalemons@Lisas-MacBook-Pro.local>
2019-05-31 21:39:06 -0400

    Wrote tests for the room

--- src/app/services/booking.service.ts
+++ src/app/services/booking.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { Room } from '@models/room/room';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookingService {
-
+  rooms: Room[];
   constructor() { }
 }
